674721e8befdfa1158dfc7dcc6e3983fd9dfe5f3
widkit <exylloath@gmail.com>
2024-05-04 01:19:50 +0300

    Non-UTC date led to fetching the same image

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import datetime
+from datetime import datetime
 import os
 import subprocess
 import requests
+import pytz
 
 save_images = False
 # Uncomment below to save the images
-# save_images = True
+save_images = True
 
 
 # Check if "ascii-image-converter" is installed.
@@ -26,11 +27,15 @@ if not os.path.exists(config_dir):
     os.makedirs(config_dir)
     open(date_dir_file, 'w').close()
 
-# Assign today's date to a variable
-date = str(datetime.date.today()).replace("-", "_")
+# Assign today's date to a variable (in UTC, bing refreshes their wallpapers by UTC)
+utc_now = datetime.now(pytz.utc)
+utc_date = utc_now.date()
+date_utc = str(utc_date).replace("-", "_")
 
+
+# Check if the save_images is enabled.
 if save_images == True:
-    image_file = config_dir + f"/image_{date}.jpg"
+    image_file = config_dir + f"/image_{date_utc}.jpg"
 elif save_images == False:
     image_file = config_dir + "/image.jpg"
 
@@ -38,7 +43,7 @@ elif save_images == False:
 
 # Check if date is equal to last used date of the script. If not, fetch the new image.
 f = open(date_dir_file, "r")
-if f.read() != date:
+if f.read() != date_utc:
    f.close()
    if save_images != True:
        if os.path.isfile(image_file):
@@ -49,7 +54,7 @@ if f.read() != date:
    with open(image_file, 'wb') as handler:
        handler.write(daily_image)
    f = open(date_dir_file, "w")
-   f.write(date)
+   f.write(date_utc)
    f.close()
 
 # Convert the image to colorful ASCII using ascii-image-converter and print it.
